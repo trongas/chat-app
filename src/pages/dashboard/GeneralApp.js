ada10e330a98c239e6e3b5ddabaeb6e22a5e9d8f
trongas <trongdinh07@gmail.com>
2024-10-11 09:16:32 +0700

    project-base

---
+++ src/pages/dashboard/GeneralApp.js
@@ -0,0 +1,15 @@
+import React, { Suspense, lazy } from "react";
+
+const Cat = lazy(() => import("../../components/Cat"));
+const GeneralApp = () => {
+
+  return (
+    <>
+    <Suspense fallback="Loading...">
+      <Cat/>
+    </Suspense>
+    </>
+  );
+};
+
+export default GeneralApp;
